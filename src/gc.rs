39a59189a7650de5f170c03cc73a25411627b9b6
Lee ByeongJun <lbj199874@gmail.com>
2023-11-27 21:54:35 +0900

    gc

--- src/gc.rs
+++ src/gc.rs
@@ -1,21 +1,57 @@
 use std::fmt;
 
-use crate::{controller::PIController, vm::{VirtualMachine, OpCode}, heap::{Heap, self}, object::{ObjectAddress, ObjectTrait, Object, Field, Address}};
+use crate::{
+    controller::{PIConfig, PIController},
+    object::{ObjectAddress, ObjectTrait},
+    vm::{OpCode, VirtualMachine},
+};
 
 pub struct TriColorGC {
     pub color: TriColor,
 }
 
-#[derive(Debug, PartialEq, Clone)]
+#[derive(Debug, PartialEq, Clone, Default)]
 pub enum GCType {
+    #[default]
     MarkAndSweep,
 }
 
-#[derive(Debug, PartialEq, Clone, Copy)]
+#[derive(Debug, PartialEq, Default, Clone)]
+pub enum GCStatus {
+    #[default]
+    Idle,
+    Marking,
+    Sweeping,
+}
+
+#[derive(Debug, PartialEq, Clone, Copy, Default)]
 pub enum TriColor {
+    #[default]
     White,
     Gray,
     Black,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct GarbageCollector {
+    pub gc_typ: GCType,
+    pub gc_status: GCStatus,
+    pub confidence: f64,
+    pub pi_controller: PIController,
+}
+
+impl GarbageCollector {
+    pub fn initialize_colors(&mut self, vm: &mut VirtualMachine) {
+        vm.heap.objects.values_mut().for_each(|o| {
+            o.header.marked = TriColor::White;
+        })
+    }
+
+    pub fn update_color(&self, addr: ObjectAddress, new_color: TriColor, vm: &mut VirtualMachine) {
+        if let Some(obj) = vm.heap.objects.get_mut(&addr) {
+            obj.header.marked = new_color;
+        }
+    }
 }
 
 pub trait Marker {
@@ -32,6 +68,10 @@ pub trait Marker {
 
         while let Some(curr_addr) = grays.pop() {
             if vm.process_object(curr_addr, &mut grays) {
+                vm.op_codes.push(OpCode::Mark(
+                    curr_addr,
+                    vm.heap.objects.get(&curr_addr).unwrap().size(),
+                ));
                 continue;
             }
         }
@@ -44,6 +84,7 @@ pub trait Marker {
         for (addr, obj) in vm.heap.objects.iter_mut() {
             if obj.header.marked == TriColor::Black {
                 obj.header.marked = TriColor::White;
+                vm.op_codes.push(OpCode::Sweep);
                 sweeped += 1;
             } else {
                 freed += 1;
@@ -55,6 +96,17 @@ pub trait Marker {
     }
 }
 
+impl Default for GarbageCollector {
+    fn default() -> Self {
+        Self {
+            gc_typ: GCType::MarkAndSweep,
+            gc_status: GCStatus::Idle,
+            confidence: 0.0,
+            pi_controller: PIController::new(PIConfig::default()),
+        }
+    }
+}
+
 impl fmt::Display for TriColor {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
@@ -63,8 +115,4 @@ impl fmt::Display for TriColor {
             TriColor::Black => write!(f, "Black: Visited"),
         }
     }
-}
-
-pub struct MarkAndSweep {
-    pub pi: PIController,
 }
